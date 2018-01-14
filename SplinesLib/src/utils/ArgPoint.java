53fa805ff91d2928769f405cf31023ebf8adfd1c
noam mantin <noam mantin@gmail.com>
2018-01-14 21:53:06 +0200

    added ArgPoint (bad name)- a point with an angle

---
+++ SplinesLib/src/utils/ArgPoint.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class ArgPoint extends Point {
+
+	private double angle;
+
+	public ArgPoint(Point p, double angle) {
+		super(p);
+		this.angle = angle;
+	}
+
+	public double getAngle() {
+		return angle;
+	}
+
+	public void setAngle(double angle) {
+		this.angle = angle;
+	}
+}
