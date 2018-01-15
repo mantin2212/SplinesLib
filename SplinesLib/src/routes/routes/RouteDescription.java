c721ed09708bde47c8adc2d6f7530169d36b61f1
noam mantin <noam mantin@gmail.com>
2018-01-15 20:29:55 +0200

    added documentation to RouteDescription

--- SplinesLib/src/routes/routes/RouteDescription.java
+++ SplinesLib/src/routes/routes/RouteDescription.java
@@ -14,18 +14,42 @@ import routes.RouteFunctionData;
  */
 public abstract class RouteDescription {
 
+	/**
+	 * the following enum describes an axis, and can contain one of two values- X
+	 * and Y.
+	 * 
+	 * @author noam mantin
+	 *
+	 */
 	public enum Axis {
 		X, Y
 	}
 
+	// the start and finish points of the route
 	private ArgPoint start;
 	private ArgPoint finish;
 
+	/**
+	 * the function builds a new {@link RouteDescription} object, with received
+	 * start and finish points
+	 * 
+	 * @param start
+	 *            the point from which the route should start.
+	 * @param finish
+	 *            the point which should be the end of the route.
+	 */
 	public RouteDescription(ArgPoint start, ArgPoint finish) {
 		this.start = start;
 		this.finish = finish;
 	}
 
+	/**
+	 * calculates and returns the axis function (x(s) or y(s)) of the route.
+	 * 
+	 * @param axis
+	 *            the axis the function refers to.
+	 * @return the received axis's function of the route.
+	 */
 	public abstract DifferentiableFunction getFunction(Axis axis);
 
 	/**
@@ -47,10 +71,21 @@ public abstract class RouteDescription {
 		}
 	}
 
+	/**
+	 * returns the function data which is relevant to the X axis.
+	 * 
+	 * @return a {@link RouteFunctionData} object which contains the mentioned data.
+	 */
 	private RouteFunctionData getXData() {
 		return new RouteFunctionData(start.getX(), finish.getX(), Math.cos(start.getAngle()),
 				Math.cos(finish.getAngle()));
 	}
+
+	/**
+	 * returns the function data which is relevant to the Y axis.
+	 * 
+	 * @return a {@link RouteFunctionData} object which contains the mentioned data.
+	 */
 
 	private RouteFunctionData getYData() {
 		return new RouteFunctionData(start.getY(), finish.getY(), Math.sin(start.getAngle()),
