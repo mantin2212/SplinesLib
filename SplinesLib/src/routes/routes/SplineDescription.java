9c1a95778e0aaf9a8cad3b0e75cb9bdf8fde08e9
noam mantin <noam mantin@gmail.com>
2018-01-16 11:27:13 +0200

    added documentation to SplineDescription

--- SplinesLib/src/routes/routes/SplineDescription.java
+++ SplinesLib/src/routes/routes/SplineDescription.java
@@ -5,10 +5,25 @@ import routes.ArgPoint;
 import routes.RouteFunctionData;
 import utils.Utils;
 
+/**
+ * an implementation of {@link RouteDescription}. uses 3rd degree polynomial
+ * functions as axis functions.
+ * 
+ * @author noam mantin
+ *
+ */
 public class SplineDescription extends RouteDescription {
 
+	// k is a constant of the route, which determines the curvature of the route.
 	private double k;
 
+	/**
+	 * builds a new {@link SplineDescription}, with received fields
+	 * 
+	 * @param start
+	 * @param finish
+	 * @param k
+	 */
 	public SplineDescription(ArgPoint start, ArgPoint finish, double k) {
 		super(start, finish);
 		this.k = k;
