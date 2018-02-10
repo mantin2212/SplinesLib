89618de58f8bce4b267c595bf1848e1c973b9f0b
noam mantin <noam mantin@gmail.com>
2018-02-10 11:57:45 +0200

    added documentation to RouteFunctionData

--- SplinesLib/src/routes/RouteFunctionData.java
+++ SplinesLib/src/routes/RouteFunctionData.java
@@ -1,15 +1,41 @@
 package routes;
 
+/**
+ * the class describes the data needed to specify a 3rd degree polynomial
+ * function. it contains the values of the function at x=0,1, and it's
+ * derivative at x=0,1.
+ * 
+ * @author noam mantin
+ *
+ */
 public class RouteFunctionData {
 
+	/**
+	 * the values of the function at x=0,1
+	 */
 	private double functionAt0;
 	private double functionAt1;
 
+	/**
+	 * the derivative of the function at x=0,1
+	 */
 	private double derivativeAt0;
 	private double derivativeAt1;
 
+	/**
+	 * creates a new {@link RouteFunctionData} object, with received values
+	 * 
+	 * @param functionAt0
+	 *            the value of the specified function at x=0
+	 * @param functionAt1
+	 *            the value of the specified function at x=1
+	 * @param derivativeAt0
+	 *            the derivative of the specified function at x=0
+	 * @param derivativeAt1
+	 *            the derivative of the specified function at x=1
+	 */
 	public RouteFunctionData(double functionAt0, double functionAt1, double derivativeAt0, double derivativeAt1) {
-
+		//
 		this.functionAt0 = functionAt0;
 		this.functionAt1 = functionAt1;
 
