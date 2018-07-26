ee188898a3b6fd160994f38bdc1bc16d3da4ac49
noam mantin <noam mantin@gmail.com>
2018-07-26 11:31:25 +0300

    added documentation

--- SplinesLib/src/routes/synchronizing/RouteSpeedProvider.java
+++ SplinesLib/src/routes/synchronizing/RouteSpeedProvider.java
@@ -1,7 +1,25 @@
 package routes.synchronizing;
 
+/**
+ * this interface describes an object which supplies speeds to each point of a
+ * specific route.
+ *
+ * @author noam mantin
+ * 
+ * @see DriveMaxSpeeds
+ */
 public interface RouteSpeedProvider {
 
+	/**
+	 * returns the wanted speed in a specific point.
+	 * 
+	 * @param index
+	 *            the index of the referred point.
+	 * @return the linear speed an object (the robot, for example) should have when
+	 *         passing in the point with the given index.
+	 * 
+	 * @see RouteSynchronizer
+	 */
 	abstract double getLinearSpeed(int index);
 
 }
--- SplinesLib/src/routes/synchronizing/SpeedProviderFactory.java
+++ SplinesLib/src/routes/synchronizing/SpeedProviderFactory.java
@@ -2,7 +2,24 @@ package routes.synchronizing;
 
 import routes.utils.RoutePointInfo;
 
+/**
+ * this interface is a factory which supplies {@link RouteSpeedProvider}
+ * objects.
+ *
+ * @author noam mantin
+ * @see RouteSpeedProvider
+ */
 public interface SpeedProviderFactory {
 
+	/**
+	 * returns a speed provider according to a given route information.
+	 *
+	 * @param routeInfo
+	 *            the route to which the returned {@link RouteSpeedProvider} object
+	 *            should refer.
+	 * @return a {@link RouteSpeedProvider} object which activates on the given
+	 *         route.
+	 * @see RouteSpeedProvider
+	 */
 	abstract RouteSpeedProvider getSpeedProvider(RoutePointInfo[] routeInfo);
 }
